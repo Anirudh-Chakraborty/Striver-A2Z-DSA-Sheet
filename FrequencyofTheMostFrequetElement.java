0a64d296cde3b95a766000fcedd410708c777793
anirudhchakraborty <preetiujjal@gmail.com>
2026-01-28 23:33:23 +0530

    Frequency of the Most Frequent Element included Sliding Window Algo

---
+++ FrequencyofTheMostFrequetElement.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class FrequencyofTheMostFrequetElement {
+    public static void main(String[] args) {
+        int nums [] = {3,9,6};
+        int k = 2;
+
+        System.out.println(maxFrequency(nums, k));
+    }
+    public static int maxFrequency(int[] nums, int k) {
+     
+        int greatest = nums[0];
+        int maxFrequency = 0;
+        for(int i = 0; i<nums.length; i++){
+            if (nums[i]>greatest) {
+                greatest = nums[i];
+            }
+        }
+        System.out.println(greatest + "Is the Greatest");
+    
+        for(int i = 0; i< nums.length; i++){
+            if (greatest - nums[i] <= k) {
+               nums[i] = nums[i] + (greatest - nums[i]);
+               k = k - (greatest- nums[i]); 
+            }
+        }
+
+        for(int i = 0; i<nums.length; i++){
+            if (nums[i]==greatest) {
+                maxFrequency++;
+            }
+        }
+
+        // while (k>=0) {
+        //     for(int i = 0; i<nums.length; i++){
+
+        //     }
+        // }
+
+
+        System.out.println(Arrays.toString(nums));
+        return maxFrequency;
+    }
+}
